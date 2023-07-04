0d5e42bfaf5ff4ef73f9b00e97aa0facc54d2007
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-07-04 22:50:03 +0200

    Creating a constructor

--- ThrowExepction/main2.php
+++ ThrowExepction/main2.php
@@ -1,17 +1,24 @@
 <?php
 
-class Plant {
+class Plant
+{
     private float $height;
     private string $plantColor;
     private string $flowerColor;
 
-    public function printIfHigherThan(float $height) {
-        if($this->height > $height) {
+    public function __construct(float $height, string $plantColor, string $flowerColor)
+    {
+        $this->height = $height;
+        $this->plantColor = $plantColor;
+        $this->flowerColor = $flowerColor;
+    }
+
+    public function printIfHigherThan(float $height)
+    {
+        if ($this->height > $height) {
             echo "The plant is higher than the given height.";
-        }else {
+        } else {
             echo "The plant your looking for it too short.";
         }
     }
 }
-
-
