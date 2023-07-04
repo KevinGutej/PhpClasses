3a76f11af8796898ac86630eaa161c9cf1fec183
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-07-04 22:49:37 +0200

    Creating class Plant

--- ThrowExepction/main2.php
+++ ThrowExepction/main2.php
@@ -1,1 +1,17 @@
+<?php
+
+class Plant {
+    private float $height;
+    private string $plantColor;
+    private string $flowerColor;
+
+    public function printIfHigherThan(float $height) {
+        if($this->height > $height) {
+            echo "The plant is higher than the given height.";
+        }else {
+            echo "The plant your looking for it too short.";
+        }
+    }
+}
+
 
